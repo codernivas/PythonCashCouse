bcac848765cad5cbed00829313ff5134e77eb07c
Nivas BN <>
2022-12-28 08:41:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#input Statement
+para=[]
+print("Enter a para : ")
+
+while True:
+  line=input()
+  if line:
+    para.append(line)
+  else:
+    break
+
+print(para)
+output='\n'.join(para)
+print(output)
