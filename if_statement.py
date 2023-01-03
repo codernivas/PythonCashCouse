6940db56b20e67797869408232dc6c91967bb4b1
Nivas BN <121162928+codernivas@users.noreply.github.com>
2023-01-03 05:52:00 +0000

    nested if

--- if_statement.py
+++ if_statement.py
@@ -67,3 +67,40 @@ i = 2
 while i <= 20:
   print(i)
   i += 2
+
+#Range 
+
+print(list(range(5)))
+print(list(range(2,5)))
+print(list(range(0,21,5)))
+print(list(range(1,20,2)))
+
+#For loop
+
+for i in range(0, 21, 2):
+ print(i)
+
+for i in range(5):
+  a=int(input("Enter a no : "))
+  b=int(input("Enter a no : "))
+  print(a+b)
+
+#Nested for
+
+for i in range(6):
+  for j in range(i):
+    print("*",end="")
+  print("")
+print("---------------")
+
+for i in range(5,0,-1):
+  for j in range(i):
+    print("*",end="")
+  print("")
+print("---------------")
+
+for i in range(65,70,1):
+  for j in range(65,70,1):
+    print(chr(j),end="")
+  print("")
+
--- main.py
+++ main.py
@@ -70,14 +70,4 @@ print(output)
 #Casting Type
 
 #If Statement
-i = 1
-while i <= 10:
-  print(i)
-  i += 1
-print("-----------------")
-print("Even No : ")
-n = 20
-i = 2
-while i <= 20:
-  print(i)
-  i += 2
+
